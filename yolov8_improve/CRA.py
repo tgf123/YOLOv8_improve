911a581829300657ea047a2e0b164df028a1ad25
一碗汤饭 <84762793+tgf123@users.noreply.github.com>
2024-10-30 20:03:34 +0800

    Add files via upload

---
+++ yolov8_improve/CRA.py
@@ -0,0 +1,284 @@
+import torch.nn as nn
+import math
+from timm.models.layers import trunc_normal_
+import torch
+
+
+
+# 定义一个通道压缩注意力模块类
+class ChannelReductionAttention(nn.Module):
+    def __init__(self, dim1, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., pool_ratio=2):
+        super().__init__()
+
+        # 确保dim1可以被head数量整除
+        assert dim1 % num_heads == 0, f"dim {dim1} should be divided by num_heads {num_heads}."
+
+        self.dim1 = dim1
+        self.pool_ratio = pool_ratio  # 用于池化的比例
+        self.num_heads = num_heads  # 注意力头数
+        head_dim = dim1 // num_heads  # 每个注意力头的维度
+
+        # 设置缩放因子，如果未提供qk_scale，则使用head_dim的倒数平方根
+        self.scale = qk_scale or head_dim ** -0.5
+
+        # 定义查询（q）、键（k）、值（v）的线性层
+        self.q = nn.Linear(dim1, self.num_heads, bias=qkv_bias)
+        self.k = nn.Linear(dim1, self.num_heads, bias=qkv_bias)
+        self.v = nn.Linear(dim1, dim1, bias=qkv_bias)
+
+        # 定义注意力和投影的dropout层
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim1, dim1)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+        # 定义池化和卷积操作，平均池化降低空间维度，卷积保持通道数
+        self.pool = nn.AvgPool2d(pool_ratio, pool_ratio)
+        self.sr = nn.Conv2d(dim1, dim1, kernel_size=1, stride=1)  # 1x1卷积保持输入和输出通道一致
+
+        # 定义LayerNorm和激活函数
+        self.norm = nn.LayerNorm(dim1)
+        self.act = nn.GELU()
+
+        # 初始化权重
+        self.apply(self._init_weights)
+
+    # 定义初始化函数，适用于线性层、LayerNorm和卷积层
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)  # 截断正态分布初始化
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)  # 偏置初始化为0
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)  # LayerNorm的偏置初始化为0
+            nn.init.constant_(m.weight, 1.0)  # LayerNorm的权重初始化为1
+        elif isinstance(m, nn.Conv2d):
+            # 使用Kaiming方法初始化卷积层
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()  # 偏置初始化为0
+
+    # 前向传播过程
+    def forward(self, x):
+
+        n_, _, h_, w_ = x.shape
+
+        x = x.flatten(2).transpose(1, 2)
+
+        B, N, C = x.shape  # 获取batch大小，序列长度和通道数
+
+        # 计算查询q，将输入x通过线性层生成多头的查询向量
+        q = self.q(x).reshape(B, N, self.num_heads).permute(0, 2, 1).unsqueeze(-1)
+
+        # 将输入x调整为卷积所需的形状，并通过池化和卷积层处理
+        x_ = x.permute(0, 2, 1).reshape(B, C, h_, w_)
+        x_ = self.sr(self.pool(x_)).reshape(B, C, -1).permute(0, 2, 1)
+
+        # 归一化并激活处理后的x_
+        x_ = self.norm(x_)
+        x_ = self.act(x_)
+
+        # 计算键k和值v，类似于查询q的过程
+        k = self.k(x_).reshape(B, -1, self.num_heads).permute(0, 2, 1).unsqueeze(-1)
+        v = self.v(x_).reshape(B, -1, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+
+        # 计算注意力得分，使用缩放因子进行缩放，然后应用softmax
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)  # 加入dropout
+
+        # 将注意力分数和v相乘，得到注意力加权输出
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+
+        # 投影输出并添加投影的dropout
+        x = self.proj(x)
+        x = self.proj_drop(x)
+
+        x = x.permute(0, 2, 1).reshape(n_, -1, h_, w_)
+        return x
+
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+
+
+class Bottleneck_CRA(nn.Module):
+    """Standard bottleneck."""
+
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        """Initializes a standard bottleneck module with optional shortcut connection and configurable parameters."""
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.cv2 = ChannelReductionAttention(c_)
+        self.add = shortcut and c1 == c2
+
+    def forward(self, x):
+        """Applies the YOLO FPN to input data."""
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+
+
+class C2f_CRA(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initializes a CSP bottleneck with 2 convolutions and n Bottleneck blocks for faster processing."""
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck_CRA(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+if __name__ == '__main__':
+    CRA = ChannelReductionAttention(256)
+    #创建一个输入张量
+    batch_size = 8
+    input_tensor=torch.randn(batch_size, 256, 64, 64 )
+    #运行模型并打印输入和输出的形状
+    output_tensor =CRA(input_tensor)
+    print("Input shape:",input_tensor.shape)
+    print("0utput shape:",output_tensor.shape)
+
+
+
+# # Ultralytics YOLO 🚀, AGPL-3.0 license
+# # YOLOv8 object detection model with P3-P5 outputs. For Usage examples see https://docs.ultralytics.com/tasks/detect
+#
+# # Parameters
+# nc: 80 # number of classes
+# scales: # model compound scaling constants, i.e. 'model=yolov8n.yaml' will call yolov8.yaml with scale 'n'
+#   # [depth, width, max_channels]
+#   n: [0.33, 0.25, 1024] # YOLOv8n summary: 225 layers,  3157200 parameters,  3157184 gradients,   8.9 GFLOPs
+#   s: [0.33, 0.50, 1024] # YOLOv8s summary: 225 layers, 11166560 parameters, 11166544 gradients,  28.8 GFLOPs
+#   m: [0.67, 0.75, 768] # YOLOv8m summary: 295 layers, 25902640 parameters, 25902624 gradients,  79.3 GFLOPs
+#   l: [1.00, 1.00, 512] # YOLOv8l summary: 365 layers, 43691520 parameters, 43691504 gradients, 165.7 GFLOPs
+#   x: [1.00, 1.25, 512] # YOLOv8x summary: 365 layers, 68229648 parameters, 68229632 gradients, 258.5 GFLOPs
+#
+# # YOLOv8.0n backbone
+# backbone:
+#   # [from, repeats, module, args]
+#   - [-1, 1, Conv, [64, 3, 2]] # 0-P1/2     1x3x640x640 -> 1x16x320x320
+#   - [-1, 1, Conv, [128, 3, 2]] # 1-P2/4   1x16x320x320 -> 1x32x160x160
+#   - [-1, 3, C2f, [128, True]]      #1x32x160x160 -> 1x32x160x160
+#   - [-1, 1, Conv, [256, 3, 2]] # 3-P3/8    1x32x160x160 -> 1x64x80x80
+#   - [-1, 6, C2f, [256, True]]              # 1x64x80x80 -> 1x64x80x80
+#   - [-1, 1, Conv, [512, 3, 2]] # 5-P4/16    1x64x80x80 -> 1x128x40x40
+#   - [-1, 6, C2f, [512, True]]             #1x128x40x40-> 1x128x40x40
+#   - [-1, 1, Conv, [1024, 3, 2]] # 7-P5/32  1x128x40x40-> 1x256x20x20
+#   - [-1, 3, C2f, [1024, True]]            # 1x256x20x20-> 1x256x20x20
+#   - [-1, 1, SPPF, [1024, 5]] # 9             1x256x20x20-> 1x256x20x20
+#
+# # YOLOv8.0n head
+# head:
+#   - [-1, 1, nn.Upsample, [None, 2, "nearest"]]   # 1x256x40x40
+#   - [[-1, 6], 1, Concat, [1]] # cat backbone P4  # # 1x384x40x40
+#   - [-1, 3, C2f, [512]] # 12                       1x128x40x40
+#
+#   - [-1, 1, nn.Upsample, [None, 2, "nearest"]] #   1x128x80x80
+#   - [[-1, 4], 1, Concat, [1]] # cat backbone P3    1x192x80x80
+#   - [-1, 3, C2f_CRA, [256]] # 15 (P3/8-small)          1x64x80x80
+#
+#   - [-1, 1, Conv, [256, 3, 2]]                     #1x64x40x40
+#   - [[-1, 12], 1, Concat, [1]] # cat head P4        #1x192x40x40
+#   - [-1, 3, C2f_CRA, [512]] # 18 (P4/16-medium)       #1x128x40x40
+#
+#   - [-1, 1, Conv, [512, 3, 2]]                     #1x128x20x20
+#   - [[-1, 9], 1, Concat, [1]] # cat head P5        #1x384x20x20
+#   - [-1, 3, C2f_CRA, [1024]] # 21 (P5/32-large)       #1x256x20x20
+#
+#   - [[15, 18, 21], 1, Detect, [nc]] # Detect(P3, P4, P5)
+#
+
+
+
+
+# # Ultralytics YOLO 🚀, AGPL-3.0 license
+# # YOLOv8 object detection model with P3-P5 outputs. For Usage examples see https://docs.ultralytics.com/tasks/detect
+#
+# # Parameters
+# nc: 80 # number of classes
+# scales: # model compound scaling constants, i.e. 'model=yolov8n.yaml' will call yolov8.yaml with scale 'n'
+#   # [depth, width, max_channels]
+#   n: [0.33, 0.25, 1024] # YOLOv8n summary: 225 layers,  3157200 parameters,  3157184 gradients,   8.9 GFLOPs
+#   s: [0.33, 0.50, 1024] # YOLOv8s summary: 225 layers, 11166560 parameters, 11166544 gradients,  28.8 GFLOPs
+#   m: [0.67, 0.75, 768] # YOLOv8m summary: 295 layers, 25902640 parameters, 25902624 gradients,  79.3 GFLOPs
+#   l: [1.00, 1.00, 512] # YOLOv8l summary: 365 layers, 43691520 parameters, 43691504 gradients, 165.7 GFLOPs
+#   x: [1.00, 1.25, 512] # YOLOv8x summary: 365 layers, 68229648 parameters, 68229632 gradients, 258.5 GFLOPs
+#
+# # YOLOv8.0n backbone
+# backbone:
+#   # [from, repeats, module, args]
+#   - [-1, 1, Conv, [64, 3, 2]] # 0-P1/2     1x3x640x640 -> 1x16x320x320
+#   - [-1, 1, Conv, [128, 3, 2]] # 1-P2/4   1x16x320x320 -> 1x32x160x160
+#   - [-1, 3, C2f, [128, True]]      #1x32x160x160 -> 1x32x160x160
+#   - [-1, 1, Conv, [256, 3, 2]] # 3-P3/8    1x32x160x160 -> 1x64x80x80
+#   - [-1, 6, C2f, [256, True]]              # 1x64x80x80 -> 1x64x80x80
+#   - [-1, 1, Conv, [512, 3, 2]] # 5-P4/16    1x64x80x80 -> 1x128x40x40
+#   - [-1, 6, C2f, [512, True]]             #1x128x40x40-> 1x128x40x40
+#   - [-1, 1, Conv, [1024, 3, 2]] # 7-P5/32  1x128x40x40-> 1x256x20x20
+#   - [-1, 3, C2f, [1024, True]]            # 1x256x20x20-> 1x256x20x20
+#   - [-1, 1, ChannelReductionAttention, []]
+#   - [-1, 1, SPPF, [1024, 5]] # 9             1x256x20x20-> 1x256x20x20
+#
+# # YOLOv8.0n head
+# head:
+#   - [-1, 1, nn.Upsample, [None, 2, "nearest"]]   # 1x256x40x40
+#   - [[-1, 6], 1, Concat, [1]] # cat backbone P4  # # 1x384x40x40
+#   - [-1, 3, C2f, [512]] # 12                       1x128x40x40
+#
+#   - [-1, 1, nn.Upsample, [None, 2, "nearest"]] #   1x128x80x80
+#   - [[-1, 4], 1, Concat, [1]] # cat backbone P3    1x192x80x80
+#   - [-1, 3, C2f, [256]] # 15 (P3/8-small)          1x64x80x80
+#
+#   - [-1, 1, Conv, [256, 3, 2]]                     #1x64x40x40
+#   - [[-1, 13], 1, Concat, [1]] # cat head P4        #1x192x40x40
+#   - [-1, 3, C2f, [512]] # 18 (P4/16-medium)       #1x128x40x40
+#
+#   - [-1, 1, Conv, [512, 3, 2]]                     #1x128x20x20
+#   - [[-1, 10], 1, Concat, [1]] # cat head P5        #1x384x20x20
+#   - [-1, 3, C2f, [1024]] # 21 (P5/32-large)       #1x256x20x20
+#
+#   - [[16, 19, 22], 1, Detect, [nc]] # Detect(P3, P4, P5)
+
+
+# elif m is ChannelReductionAttention:
+#     args = [ch[f]]
